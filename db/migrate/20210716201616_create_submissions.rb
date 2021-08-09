4a0222767e19c3f743fc918feb8f82ee7521221b
Alex Bruns <alex.h.bruns@gmail.com>
2021-08-09 02:55:18 -0500

    Fixed migration.

--- db/migrate/20210716201616_create_submissions.rb
+++ db/migrate/20210716201616_create_submissions.rb
@@ -14,21 +14,21 @@ class CreateSubmissions < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
+    create_table :works do |t|
+      t.belongs_to :user, foreign_key: true
+
+      t.integer :stage, null: false # draft, written
+      t.string :title, null: false
+
+      t.timestamps
+    end
+
     create_table :submissions do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :buyer, null: false, foreign_key: true
       t.belongs_to :work, null: false, foreign_key: true
 
       t.integer :status, null: false # open, accepted, rejected
-
-      t.timestamps
-    end
-
-    create_table :works do |t|
-      t.belongs_to :user, foreign_key: true
-
-      t.integer :stage, null: false # draft, written
-      t.string :title, null: false
 
       t.timestamps
     end
