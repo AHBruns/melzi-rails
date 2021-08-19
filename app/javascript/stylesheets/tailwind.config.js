aa9f3f86bcd72c0f69e7cd25ab388e182f3128eb
Alex Bruns <alex.h.bruns@gmail.com>
2021-08-18 22:34:18 -0500

    let's not delete classes we need
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: ["./app/components/**/*.*", "./app/views/**/*.*"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
+    purge: {
+        enabled: process.env.NODE_ENV !== 'development',
+        options: {
+            safelist: ["type"],
+        },
+        content: ["./app/components/**/*.*", "./app/views/**/*.*"]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/forms")],
 };
